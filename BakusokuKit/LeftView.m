f3ffe6222887b2e05c1204d34c40f5462630d98a
junishitsuka <ishitsuka.jun@gmail.com>
2014-08-28 00:36:04 +0900

    first commit

---
+++ BakusokuKit/LeftView.m
@@ -0,0 +1,13 @@
+//
+//  LeftView.m
+//  BakusokuKit
+//
+//  Created by Ishitsuka Jun on 2014/08/13.
+//  Copyright (c) 2014年 Ishitsuka Jun. All rights reserved.
+//
+
+#import "LeftView.h"
+
+@implementation LeftView
+
+@end
